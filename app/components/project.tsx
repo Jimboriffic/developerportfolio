7834441d234dd06b86fea6985e8f9df3762dc701
DeCodeerMeneer <james.sliwa@gmail.com>
2024-01-08 09:00:14 +0100

    Update project.tsx
    images should be hidden on smaller screens

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -66,7 +66,7 @@ export default function Project({
           src={imageUrl}
           alt="project I worked on"
           quality={90}
-          className="absolute top-8 -right-40 w-[28.25rem]
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem]
     rounded-t-lg shadow-2xl 
     transition
     group-hover:scale-[1.04]
